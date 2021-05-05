7390cd7644b9183bdd9029e598e6981907c35258
elijahdaniel <e.paminsan@gmail.com>
2021-05-04 17:51:30 -0700

    complete with tests passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -40,4 +40,68 @@ router.post('/', async (req, res) => {
   }
 })
 
+// PUT /api/posts/:id
+router.put('/:id', async (req, res) => {
+  db.update(req.params.id, req.body)
+    .then(post => {
+      if (
+        !req.body.title ||
+        !req.body.contents ||
+        req.body.title === '' ||
+        req.body.contents === ''
+      ) {
+        res
+          .status(400)
+          .json({ message: 'Please provide title and contents for the post' })
+      } else if (!post) {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' })
+      } else {
+        res.status(200).json({ id: Number(req.params.id), ...req.body })
+      }
+    })
+    .catch(_ =>
+      res
+        .status(500)
+        .json({ message: 'The post information could not be modified' })
+    )
+})
+
+// DELETE /api/posts/:id
+router.delete('/:id', async (req, res) => {
+  const deleted = await db.findById(req.params.id)
+
+  try {
+    if (deleted) {
+      res.status(200).json(deleted)
+      await db.remove(req.params.id)
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// GET /api/posts/:id/comments
+router.get('/:id/comments', async (req, res) => {
+  const checkID = await db.findById(req.params.id)
+  const getPosts = await db.findPostComments(req.params.id)
+
+  try {
+    !checkID
+      ? res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' })
+      : res.status(200).json(getPosts)
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'The comments information could not be retrieved' })
+  }
+})
+
 module.exports = router
